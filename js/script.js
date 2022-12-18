497fd321ac83de3d41177d9ce91b2cc5cd50ef51
Kacper Lechicki <klechicki@poczta.fm>
2022-12-18 23:09:04 +0100

    elements for js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const funds = document.querySelector('.available-money');
+const addTransactionBtn = document.querySelector('.add-transaction');
+const deleteAllBtn = document.querySelector('.delete-all');
+const deleteBtn = document.querySelector('.delete');
+const lightBtn = document.querySelector('.light');
+const darkBtn = document.querySelector('.dark');
+
+const addTransactionPanel = document.querySelector('.add-transaction-panel');
+
+const incomeSection = document.querySelector('.income-area');
+const expenseSection = document.querySelector('.expenses-area');
+
+const newTransactionName = document.querySelector('#name');
+const newTransactionAmount = document.querySelector('#amount');
+const newTransactionCategory = document.querySelector('#category');
+
+const saveBtn = document.querySelector('.save');
+const cancelBtn = document.querySelector('.cancel');
+
+let root = document.documentElement;
+let ID = 0;
+let categoryIcon;
+let selectedCategory;
+
+let moneyArr = [0];
